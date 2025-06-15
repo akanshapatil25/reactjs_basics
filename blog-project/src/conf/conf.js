06a446bc3b1eff901355cc51479c0317710cfa3e
akanshapatil25 <akapatil25@gmail.com>
2025-06-15 20:33:22 +0000

    fixed tinymce api key issue

--- blog-project/src/conf/conf.js
+++ blog-project/src/conf/conf.js
@@ -4,6 +4,7 @@
     appwriteDatabase: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollection: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucket: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinymceApiKey: String(import.meta.env.TINYMCE_API_KEY),
  }
 
  export default conf;
